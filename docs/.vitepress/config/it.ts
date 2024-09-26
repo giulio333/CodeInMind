8e8e5a339a72a759c4ea6e0ce707e1e5d1f76c5c
giulio333 <digiagiulio@gmail.com>
2024-09-26 21:39:26 +0000

    minor

--- docs/.vitepress/config/it.ts
+++ docs/.vitepress/config/it.ts
@@ -91,10 +91,7 @@ export const itConfig = {
                 { text: 'ASCII', link: '/it/learn/programming_languages/python/strings/ASCII' },
                 { text: 'Unicode', link: '/it/learn/programming_languages/python/strings/unicode' },
                 { text: 'Stringhe Python', link: '/it/learn/programming_languages/python/strings/python_strings' },
-<<<<<<< HEAD
                 { text: 'Metodi Stringhe', link: '/it/learn/programming_languages/python/strings/strings_method' },
-=======
->>>>>>> 91240259943657d496b5b4ef6687d84476781e2c
               ]
             },
             { text: 'Errori ed Eccezioni', link: '/it/learn/programming_languages/python/errors_and_exceptions' },
